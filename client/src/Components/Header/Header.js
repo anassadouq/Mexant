203776b48b78564b5bdd7197f4a78db3c22a36e6
anas sadouq <sadouqanas1@gmail.com>
2023-12-31 22:44:06 +0100

    add icons

--- client/src/Components/Header/Header.js
+++ client/src/Components/Header/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link} from 'react-router-dom';
+
 export default function Header(){
     return(
         <div className="container">
             <Link class="navbar-brand" to="/">
-                <img src="./images/freedom.png" width="100px"/>    
+                <img src="./images/logo.png" width="150px"/>    
             </Link>    
         </div>
     )
--- client/src/Components/Liste/Liste.js
+++ client/src/Components/Liste/Liste.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import { IoIosAddCircleOutline } from "react-icons/io";
+import { FaRegEdit } from "react-icons/fa";
+import { MdDeleteForever } from "react-icons/md";
 import './App.css';
 
 export default function Liste() {
-    const [freedoms, setFreedoms] = useState([])
+  const [freedoms, setFreedoms] = useState([])
     useEffect(() => {
       fetchFreedoms();
     },[]
@@ -12,18 +15,28 @@ export default function Liste() {
     const fetchFreedoms = async () => {
       await axios.get('http://127.0.0.1:8000/api/freedoms').then(({ data }) => {setFreedoms(data)}) 
     }
+    
     const deleteProduct = async (id) => {
-      await axios.delete('http://127.0.0.1:8000/api/freedoms/' + id)
-      .then(({ data }) => {
-        console.log(data.message)
-        fetchFreedoms();
-      }).catch(({ response: { data } }) => {
-        console.log(data.message)
-      })
-    }
+      const confirmDelete = window.confirm('Are you sure you want to delete this item?');
+      if (confirmDelete) {
+        try {
+          await axios.delete(`http://127.0.0.1:8000/api/freedoms/${id}`)
+            .then(({ data }) => {
+              console.log(data.message);
+              fetchFreedoms();
+            })
+            .catch(({ response: { data } }) => {
+              console.log(data.message);
+            });
+        } catch (error) {
+          console.error('Error deleting item:', error);
+        }
+      }
+    };
+    
     return(
       <div>
-        <Link class="mx-5 my-3 btn btn-secondary" to="/Create">Create</Link>
+        <Link class="mx-5 my-3 btn btn-warning" to="/Create"><IoIosAddCircleOutline/> Create</Link>
         <table className="container my-4">
           <tr>
             <th>Name</th>
@@ -41,8 +54,8 @@ export default function Liste() {
                   <td>{row.phone}</td>
                   <td>{row.adresse}</td>
                   <td>
-                    <Link className="btn btn-success" to={`/Update/${row.id}`}>Edit</Link>
-                    <button className="btn btn-danger mx-3" onClick={() => deleteProduct(row.id)}>Delete</button>
+                    <Link className="btn btn-secondary" to={`/Update/${row.id}`}><FaRegEdit/> Edit</Link>
+                    <button className="btn btn-danger mx-3" onClick={() => deleteProduct(row.id)}><MdDeleteForever/>Delete</button>
                   </td>       
                 </tr>
               ))
