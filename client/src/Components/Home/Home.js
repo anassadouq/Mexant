7bbff2dc8074af1317d31b8e9a3c76c980b51627
anas sadouq <sadouqanas1@gmail.com>
2024-01-21 20:50:56 +0100

    change the home text

--- client/src/Components/Home/Home.js
+++ client/src/Components/Home/Home.js
@@ -13,13 +13,13 @@ export default function Home(){
                                     <div className="header-text">
                                         <h2>Get <em>ready</em> for your <br/>business<br/>& upgrade <em>all aspects</em></h2>
                                         <div className="div-dec"></div>
-                                        <p>Mexant HTML5 Template is provided for free of charge. This layout is based on Boostrap 5 CSS framework. Anyone can download and edit for any professional website. Thank you for visiting TemplateMo website.</p>
+                                        <p>A business website can be as simple as a page with your business's brand and contact details to a complete online store. Some benefits of having a website for your business include: having a digital shopfront for your business. reaching a global audience.</p>
                                         <div className="buttons">
                                             <div className="btn btn-success">
-                                                <a href="#" className='text-light'>Discover More</a>
+                                                <a href="#" className='text-light my-5'>Discover More</a>
                                             </div>
                                             <div className="btn orange-button">
-                                                <a href="/create" className='text-light'>Contact Us</a>
+                                                <a href="/create" className='text-light my-2'>Contact Us</a>
                                             </div>
                                         </div>
                                     </div>
