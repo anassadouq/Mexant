4bc8d261bece9aeb4dcc15fab56948e1974e1bff
anas sadouq <sadouqanas1@gmail.com>
2025-03-09 21:12:02 +0000

    add link in footer

--- client/src/Components/Footer/Footer.js
+++ client/src/Components/Footer/Footer.js
@@ -4,8 +4,8 @@ export default function Footer(){
     return(
         <div className="bg-dark text-light">
             <p className="text-center">
-                <br/>Copyright © 2024. All Rights Reserved.<br/>
-                Designed by <a style={{"color":"#FF4500"}}>Anas Sadouq <br/></a>
+                <br/>Copyright © 2025. All Rights Reserved.<br/>
+                Designed by <a style={{"color":"#FF4500"}}><a href="https://anassadouq.com" target="_blank">Anas Sadouq </a><br/></a>
                 <br/>
             </p>
         </div>
